e558560ca4e6a1544e372e1e476bdb3d65cf3dfb
robertarcaleanu <alumnerobert@gmail.com>
2024-04-03 22:07:10 +0200

    Additional changes

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -5,9 +5,9 @@ from steps.model_train import train_model
 from steps.evaluation import evaluate_model
 
 
-@pipeline
+@pipeline(enable_cache=True)
 def train_pipeline(data_path: str):
     df = ingest_df(data_path)
-    clean_df(df)
-    train_model()
-    evaluate_model(df)
+    X_train, X_test, y_train, y_test = clean_df(df)
+    model = train_model(X_train, X_test, y_train, y_test)
+    r2_score, rsme = evaluate_model(model, X_test, y_test)
