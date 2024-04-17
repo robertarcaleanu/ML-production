020bfea6e0fcce6cb44c5fc7c0d599c991606680
robertarcaleanu <alumnerobert@gmail.com>
2024-04-17 21:54:57 +0200

    started inferene and training pipelines

--- pipelines/deployment_pipeline.py
+++ pipelines/deployment_pipeline.py
@@ -1,8 +1,24 @@
 def inference_pipeline(input_data):
+    """Pipeline used to predict a value
+
+    Args:
+        input_data (_type_): input data
+
+    Returns:
+        _type_: predicted result
+    """
     
     return f"Your data has been processed: {input_data.feature1} and {input_data.feature2}"
 
 def continuous_deployment_pipeline(min_accuracy):
+    """In this function we train the model and save it if the accuracy is higher than the threshold
+
+    Args:
+        min_accuracy (_type_): minimum accuracy required to save the model
+
+    Returns:
+        _type_: _description_
+    """
     # min_accuracy = 0.85
     accuracy = 0.5
     if accuracy > min_accuracy:
